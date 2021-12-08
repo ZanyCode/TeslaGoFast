d27fae50260b2d342389e40c24d95edefcc153cd
Tobias Rempel <tobias.rempel@inlogy.de>
2021-12-08 20:47:59 +0100

    added initial version of frontend and backend

---
+++ backend/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+import uvicorn
+import cv2
+import io
+from starlette.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from os.path import abspath, join, dirname
+
+# Python relies on the registry to guess mimetypes. Seems to be broken in some cases (see https://github.com/encode/starlette/issues/829). 
+# This is a workaround to guarantee correct mime type for js files. Necessary for hosting angular frontend app.
+import mimetypes
+mimetypes.init()
+mimetypes.add_type('application/javascript', '.js')
+
+DIR_BACKEND = abspath(join(dirname(abspath(__file__))))
+DIR_STATIC_FILES = abspath(join(DIR_BACKEND, "..", "frontend", "dist", "teslagofast"))
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+camera = cv2.VideoCapture(0)
+
+app.mount("/static", StaticFiles(directory=DIR_STATIC_FILES), name="static")
+
+@app.get("/api/cam")
+async def get_current_image():
+    # camera = cv2.VideoCapture(0)
+    return_value, image = camera.read()
+    res, im_png = cv2.imencode(".png", image)
+    # del(camera)
+    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")    
+
+@app.get("/api/save")
+async def save_current_image():
+    return_value, image = camera.read()
+    cv2.imwrite('snapshot.png', image)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    del(camera)
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'teslagofast';
+  imageToShow: string | ArrayBuffer | null = '';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.reloadImageLoop();
+  }
+
+  takeSnapshot(): void {
+    this.http.get(`${environment.api}/save`).subscribe();
+  }
+
+  reloadImageLoop(): void {
+    this.http
+        .get(`${environment.api}/cam`, { responseType: 'blob' })
+        .subscribe(image => {
+          let reader = new FileReader(); 
+          reader.addEventListener("load", () => {
+              this.imageToShow = reader.result; 
+          }, false);
+      
+          if (image) {
+              reader.readAsDataURL(image);
+          }
+
+          setTimeout(() => this.reloadImageLoop(), 1);
+        })
+  }
+}
